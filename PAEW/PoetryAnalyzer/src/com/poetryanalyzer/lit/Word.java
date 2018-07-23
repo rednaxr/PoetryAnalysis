ed50378b1fb3a86390394c9dbdf6b4b3042bbc2d
StudentSydney <sydv793@gmail.com>
2018-07-23 12:44:55 -0700

    create word list class

--- PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Word.java
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Word.java
@@ -1,10 +1,26 @@
 package com.poetryanalyzer.lit;
 
 public class Word {
-	private String wordText;
+	private String text;
 	private int count;
 	
-	public Word () {
-		
+	Word(String str){
+		text = str;
+	}
+	Word(String str, int wordCount){
+		text = str;
+		count = wordCount;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
 	}
 }
