ba8fa91e4ad93687f7c0e887dcf037c552814197
StudentSydney <sydv793@gmail.com>
2018-07-23 14:33:14 -0700

    fix iteration

--- PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/StructuralAnalysis.java
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/StructuralAnalysis.java
@@ -2,17 +2,32 @@ package com.poetryanalyzer.lit;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class StructuralAnalysis {
-
+    public ArrayList<Word> wordList = new ArrayList<Word> ();
 	public StructuralAnalysis(ArrayList<String> poem) {
-		var inputWords = new ArrayList<String>(Arrays.asList(str.split(" ")));
-		ArrayList<Word> wordList = new ArrayList<Word>();
-		for (String item : inputWords) 
-		{ 
-		    wordList.add(new Word(item));
+		for(String line:poem) {
+			AnalyzeLine(line);
 		}
 	}
-	
+	public void AnalyzeLine(String line) {
+		String currentWord ="";
+		ArrayList<Word> lineList = new ArrayList();
+		for (int i = 0; i < line.length(); i++){
+		    char c = line.charAt(i);        
+		    if(c == ' ') {
+		    	//TODO: search lexical database
+		    	var word = new Word(currentWord);
+		    	
+		    	wordList.add(word);
+		    	currentWord = "";
+		    }
+		    else {
+		    	
+		    }
+		    //TODO word counts
+		}
+	}
 	
 }
