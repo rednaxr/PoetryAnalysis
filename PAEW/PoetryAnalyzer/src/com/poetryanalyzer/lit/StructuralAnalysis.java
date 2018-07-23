9f669b4804cb82a2674bff221782bd63222ed293
StudentSydney <sydv793@gmail.com>
2018-07-23 13:36:24 -0700

    move to struct analysis

--- PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/StructuralAnalysis.java
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/StructuralAnalysis.java
@@ -1,11 +1,17 @@
 package com.poetryanalyzer.lit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StructuralAnalysis {
 
 	public StructuralAnalysis(ArrayList<String> poem) {
-		
+		var inputWords = new ArrayList<String>(Arrays.asList(str.split(" ")));
+		ArrayList<Word> wordList = new ArrayList<Word>();
+		for (String item : inputWords) 
+		{ 
+		    wordList.add(new Word(item));
+		}
 	}
 	
 	
