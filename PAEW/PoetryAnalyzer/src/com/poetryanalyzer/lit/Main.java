3fec1c6c1bef6bac23df7fb9d9417d0bd491a8ea
Alex Guitteau <alexanderguitteau@gmail.com>
2018-07-23 11:46:06 -0700

    implemented file reading functionality
    -added readFile method
    -added ArrayList<String> poetryLines instance variable

--- PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Main.java
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Main.java
@@ -7,13 +7,22 @@ package com.poetryanalyzer.lit;
 
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+<<<<<<< HEAD
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+=======
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+>>>>>>> 398521806fb6da06b6c58313d5c5588696868bbf
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class Main {
+public class Main implements ActionListener {
 
 	JFrame window = new JFrame("Poetry Analyzer");
 
@@ -24,12 +33,16 @@ public class Main {
 	JTextField outputTF = new JTextField("");
 	JButton runBtn = new JButton("Run");
 	
+	
+	ArrayList<String> poetryLines;
+	
+	
 	public Main() {
 		//Set up window
 		double screenWidth = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 		double screenHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 		window.setLocation((int)Math.round(screenWidth/4), (int)Math.round(screenHeight/4));
-		window.setSize(500, 250);
+		window.setSize(400, 150);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		window.setLayout(new GridLayout(3, 2));
@@ -49,6 +62,28 @@ public class Main {
 	
 	public static void main(String[] args) {
 		new Main();
+	}
+	
+	public void readFile (String filePath) {
+		File file = new File (filePath);
+		Scanner fileReader = null;
+		
+		try {
+			fileReader = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while (fileReader.hasNextLine()) {
+			poetryLines.add(fileReader.nextLine());
+		}
+		
+		fileReader.close();
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		
 	}
 
 }
--- PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/StrTools.java
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/StrTools.java
@@ -1,6 +1,11 @@
 package com.poetryanalyzer.lit;
 
-public class StrTools {
+import java.util.List;
 
+public class StrTools {
+	public static List<Word> CreateWordList(String str){
+		//var wordList = new ArrayList<String>(Arrays.asList(str.split(" ")));
+		//var WordCount 
+	}
 	
 }
