9dc688aded5ced45539d6131f62341c3e45abcf7
rednaxr <rednaxr@users.noreply.github.com>
2018-07-23 11:08:55 -0700

    further GUI builing

--- PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Main.java
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Main.java
@@ -1,7 +1,7 @@
 //Main Class of Poetry Analyzer
 //GUI and Central Control
 //by: Alexander Dyall, Alex Gitteau, & Sidney Von Arx
-
+//23 July 2018
 
 package com.poetryanalyzer.lit;
 
@@ -16,21 +16,34 @@ import javax.swing.JTextField;
 public class Main {
 
 	JFrame window = new JFrame("Poetry Analyzer");
-	JLabel inputLabel = new JLabel("Input: ");
-	JLabel outputLabel = new JLabel("Output: ");
-	JLabel statusLabel = new JLabel("Status: ");
+	JLabel inputLbl = new JLabel("Input: ");
+	JLabel outputLbl = new JLabel("Output: ");
+	JLabel statusLbl = new JLabel("Status: ");
 	JTextField inputTF = new JTextField("");
 	JTextField outputTF = new JTextField("");
 	JButton runBtn = new JButton("Run");
 	
 	public Main() {
+		//Set up window
 		double screenWidth = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 		double screenHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 		window.setLocation((int)Math.round(screenWidth/4), (int)Math.round(screenHeight/4));
-		window.setSize(500, 500);
+		window.setSize(500, 250);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		window.setLayout(new GridLayout(3, 2));
+		
+		//Set up graphical components
+		window.add(inputLbl);
+		inputLbl.setHorizontalAlignment(JLabel.CENTER);
+		window.add(inputTF);
+		window.add(outputLbl);
+		outputLbl.setHorizontalAlignment(JLabel.CENTER);
+		window.add(outputTF);
+		window.add(statusLbl);
+		statusLbl.setHorizontalAlignment(JLabel.CENTER);
+		window.add(runBtn);
+		
 	}
 	
 	public static void main(String[] args) {
