f2495d085398f28042d56ad1ef2a5bc1738bca7f
StudentSydney <sydv793@gmail.com>
2018-07-24 14:58:55 -0700

    unit test

---
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/MyTest.java
@@ -0,0 +1,20 @@
+package com.poetryanalyzer.lit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class MyTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() {
+	}
+
+	@Test
+	void test() {
+		Assert.assertEquals(true, true);
+	}
+
+}
