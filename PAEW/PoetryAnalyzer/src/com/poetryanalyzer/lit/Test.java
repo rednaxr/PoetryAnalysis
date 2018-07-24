dcb23d8f6fba0c292f42f999b2684c51828f1837
StudentSydney <sydv793@gmail.com>
2018-07-24 14:28:43 -0700

    unit test

---
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Test.java
@@ -0,0 +1,20 @@
+package com.poetryanalyzer.lit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import junit.framework.Assert;
+
+public class Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@org.junit.Test
+	public void test() {
+		assertEquals(2, 1+1);
+	}
+
+}
