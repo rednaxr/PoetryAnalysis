3797aae87f3e329e4509212e720b5a28b5fa914e
StudentSydney <sydv793@gmail.com>
2018-07-24 10:20:14 -0700

    add devicesimmediate

---
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/DevicesImmediate.java
@@ -0,0 +1,12 @@
+package com.poetryanalyzer.lit;
+
+import java.util.ArrayList;
+
+public class DevicesImmediate {
+	public ArrayList<Integer> Alliteration;
+	public ArrayList<Integer> Polysyndeton;
+	public ArrayList<Integer> Asyndeton;
+	public DevicesImmediate() {
+		
+	}
+}
