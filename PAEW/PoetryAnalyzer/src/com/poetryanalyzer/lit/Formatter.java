3d0f5f1241e081390a3c41e6c778accfdd36924f
StudentSydney <sydv793@gmail.com>
2018-07-26 15:39:44 -0700

    fix output

--- PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Formatter.java
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Formatter.java
@@ -2,7 +2,7 @@ package com.poetryanalyzer.lit;
 
 public class Formatter {
 	//s=start of a tag/section, e=end of a tag/section
-	public static final String htmlStart ="<!DOCTYPE html><title>Poem</title><link rel=\"stylesheet\" type=\"text\\css\" href=\"style.css\"><body lang =\"en\">";
+	public static final String htmlStart ="<!DOCTYPE html><title>Poem</title><style>span[data-alliteration=\"true\"] {color: red;}</style><link rel=\"stylesheet\" type=\"text\\css\" href=\"style.css\"><body lang =\"en\">";
 	public static final String htmlEnd ="</body>";
 	public static final String lineStart = "<span class=\"";
 	public static final String dataS = " data-alliteration =\"";
