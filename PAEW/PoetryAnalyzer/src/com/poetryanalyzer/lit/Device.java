7f36b65f79b7187390c23aeac2b2a85fd77fafc3
Alex Guitteau <alexanderguitteau@gmail.com>
2018-07-26 09:53:11 -0700

    Device.java and spelling corrections
    -Fleshed out Device.java class with getters and setters and intensity and indices instance variables.
    
    -corrected multiple instances of "indecies" to correct "indices".

--- PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Device.java
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Device.java
@@ -1,10 +1,29 @@
 package com.poetryanalyzer.lit;
 
+import java.util.ArrayList;
+
 public abstract class Device {
+	private ArrayList<Integer> indices; //the indices of a specific literary device within the poem
 	private int intensity; //rate of occurrence of literary device in poem
 	
 	public Device () {
 		
+	}
+	
+	public int getIntensity() {
+		return intensity;
+	}
+
+	public void setIntensity(int intensity) {
+		this.intensity = intensity;
+	}
+
+	public ArrayList<Integer> getIndices() {
+		return indices;
+	}
+
+	public void setIndices(ArrayList<Integer> indices) {
+		this.indices = indices;
 	}
 	
 }
