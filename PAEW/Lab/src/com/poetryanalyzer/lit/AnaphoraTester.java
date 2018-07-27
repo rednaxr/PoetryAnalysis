61f3db08657c93df71a12d0e116f8160fa361689
Alex Guitteau <alexanderguitteau@gmail.com>
2018-07-27 14:10:23 -0700

    Build roughly working Anaphora finder

--- PAEW/Lab/src/com/poetryanalyzer/lit/AnaphoraTester.java
+++ PAEW/Lab/src/com/poetryanalyzer/lit/AnaphoraTester.java
@@ -10,21 +10,23 @@ public class AnaphoraTester {
 
 	public static void main (String[] args) {
 		ArrayList<String> poem = new ArrayList<String>();
+		
 		poem.add("Two roads diverged in a yellow wood");
 		poem.add("And sorry I could not travel both");
 		poem.add("And be one traveler long I stood");
 		poem.add("And looked down one as far as I could");
 		poem.add("To where it bent in the undergrowth");
+		
 		Line[] poemLines = new Line[poem.size()];
+		
 		for (int x = 0; x < poemLines.length; x++) {
 			poemLines[x] = new Line (poem.get(x));
 		}
 		
-		/*for (Line x : poemLines) {
+		for (Line x : poemLines) {
 			x.displayLine();
 			System.out.println();
-		}*/
-		
+		}
 		
 		AnaphoraTester anaphoraTester = new AnaphoraTester (poemLines);
 	}
--- PAEW/Lab/src/com/poetryanalyzer/lit/MultiLineDevice.java
+++ PAEW/Lab/src/com/poetryanalyzer/lit/MultiLineDevice.java
@@ -10,8 +10,31 @@ public class MultiLineDevice extends Device {
 	
 	public static ArrayList<MultiLineDevice> checkAnaphora (Line[] lines) {
 		
-		ArrayList<MultiLineDevice> AnaphoraInstances = new ArrayList<MultiLineDevice>();
+		ArrayList<MultiLineDevice> anaphoraInstances = new ArrayList<MultiLineDevice>();
+		ArrayList<String> anaphoricWords = new ArrayList<String>();
 		
-		return AnaphoraInstances;
+		for (int i = 0; i < lines.length; i++) {
+			String firstWord = lines[i].getWords()[0].getText();
+			
+			if (!anaphoricWords.contains(firstWord)) {
+				anaphoricWords.add(firstWord);
+				anaphoraInstances.add(new MultiLineDevice());
+				anaphoraInstances.get(anaphoraInstances.size() - 1).setText(firstWord);
+				anaphoraInstances.get(anaphoraInstances.size() - 1).getIndices().add(i);;
+			} else {
+				anaphoraInstances.get(anaphoricWords.indexOf(firstWord)).getIndices().add(i);
+			}
+		  
+		}
+		
+		for (MultiLineDevice a : anaphoraInstances) {
+			System.out.println("Text" + a.getText());
+			for (int i : a.getIndices()) {
+				System.out.println("Index: " + i);
+			}
+			System.out.println();
+		}
+		
+		return anaphoraInstances;
 	}
 }
