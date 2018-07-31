22a429e89e48e7bc20b85a860fd7fd6b20974930
rednaxr <rednaxr@users.noreply.github.com>
2018-07-31 14:51:08 -0700

    developing scansion

--- PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Scansion.java
+++ PAEW/PoetryAnalyzer/src/com/poetryanalyzer/lit/Scansion.java
@@ -4,62 +4,97 @@ import java.util.ArrayList;
 
 public class Scansion {
 	
-	//Atributes
-	private byte[][] stress;						//(stores all the stresses in the poem according to this scansion)
+	//ATTRIBUTES / DYNAMIC VARIABLES
+	private boolean[][] stress;						//(stores all the stresses in the poem according to this scansion)
 	private ArrayList<ArrayList<Byte>> feet;		//(stores the feet of the poem);
 	private byte[] meterType;						//(stores the meter type (ex: loose iambic) of each line)
 	private int[] footCount;						//(stores the number of feet (ex: 4) in each line)
 	private int OverallMeterType;					//(stores type of meter, eg. loose iambic)
 	private int OverallFootCount;					//(stores number of feet per line, eg. tetrameter)
 	
-	//Feet
-	public static final byte IAMB = 1;				//   U /
-	public static final byte TROCHEE = 2;			//   / U
-	public static final byte PYRRHUS = 3;			//   U U
-	public static final byte SPONDEE = 4;			//   / /
-	public static final byte ANAPEST = 5;			//   U U /
-	public static final byte DACTYL = 6;			//   / U U
-	public static final byte AMPHIBRACH = 7;		//   U / U
-	public static final byte BACCHIUS = 8;			//   U / / 
-	public static final byte ANTIBACCHIUS = 9;		//   / / U
-	public static final byte CRETIC = 10;			//   / U /
-	public static final byte MOLOSSUS = 11;			//   / / / 
+	//STATIC CONSTANTS
+	public static final byte UNKNOWN = -1;			//   ?
 	
-	//Static Integers
-	public static final byte UNKNOWN = -1;
-	public static final byte OTHER = 0;
-	public static final byte IAMBIC = 1;
-	public static final byte LOOSE_IAMBIC = 2;
-	public static final byte ANAPESTIC = 3;
-	public static final byte TROCHAIC = 4;
-	public static final byte LOOSE_TROCHAIC = 5;
-	public static final byte DACTYLIC = 6;
+	//Feet
+	public static final byte UNSTRESS = 6;			//     U
+	public static final byte STRESS = 8;			//     /
+	
+	public static final byte PYRRHUS = 12;			//    U U
+	public static final byte IAMB = 14;				//    U /
+	public static final byte TROCHEE = 15;			//    / U
+	public static final byte SPONDEE = 17;			//    / /
+	
+	public static final byte TRIBRACH = 18;			//   U U U
+	public static final byte ANAPEST = 20;			//   U U /
+	public static final byte AMPHIBRACH = 21;		//   U / U
+	public static final byte DACTYL = 22;			//   / U U
+	public static final byte BACCHIUS = 23;			//   U / /
+	public static final byte CRETIC = 24;			//   / U /
+	public static final byte ANTIBACCHIUS = 25;		//   / / U
+	public static final byte MOLOSSUS = 27;			//   / / /
 	
 	//CONSTRUCTOR
-	public Scansion(byte[][] stress) {
+	public Scansion(boolean[][] stress) {
 		this.stress = stress;
 		setFeet(new ArrayList<ArrayList<Byte>>());
 		meterType = new byte[stress.length];
-		for(int a = 0; a < stress.length; a++) {
-			for(int b = 0; b < stress[a].length; b++) {
-				
-				if(stress[a][b] == 1) {
+	}
+	
+	//DYNAMIC METHODS---------------------------------------------------
+	
+	public void buildMeter() {
+		ArrayList<Boolean> cFoot = new ArrayList<Boolean>();		//(stores the current foot being built/worked with)
+		int cFootID = 0;												//(stores the ID of the current foot being worked with
+		for(int a = 0; a < stress.length; a++) {							//go through each line of stresses
+			for(int b = stress[a].length - 1; b > -1; b--) {
+				if(stress[a][b]) {
+					if(cFoot.size() < 3) {
+						if(stress[a][b]) {
+							for (int c = 0; c < cFoot.size(); c++) {
+								cFootID = btn(cFoot.get(c))*(c+2);
+							}
+							if(cFootID == 0) {
+								cFoot.add(stress[a][b]);
+							}
+							else {
+								cFootID += 6*cFoot.size();
+								feet.get(a).add((byte)cFootID);
+							}
+						}
+					}
+					else {
+						cFootID = 6*cFoot.size();
+						for (int c = 0; c < cFoot.size(); c++) {
+							cFootID = btn(cFoot.get(c))*(c+2);
+						}
+						feet.get(a).add((byte)cFootID);
+						cFoot.clear();
+					}
 				}
+				cFoot.add(stress[a][b]);
+				
+				cFootID = 6*cFoot.size();
+				for (int c = 0; c < cFoot.size(); c++) {
+					cFootID = btn(cFoot.get(c))*(c+2);
+				}
+				feet.get(a).add((byte)cFootID);
+				
 			}
 		}
 	}
 	
+	//returns the likelihood that this scansion is the actual scansion of the poem, as raw number
 	public int getScore() {
 		int score = 0;
 		
 		return score;
 	}
 
-	public byte[][] getStress() {
+	public boolean[][] getStress() {
 		return stress;
 	}
 
-	public void setStress(byte[][] stress) {
+	public void setStress(boolean[][] stress) {
 		this.stress = stress;
 	}
 
@@ -101,6 +136,14 @@ public class Scansion {
 
 	public void setFeet(ArrayList<ArrayList<Byte>> feet) {
 		this.feet = feet;
+	}
+	
+	//STATIC METHODS---------------------------------------------------
+	
+	public static byte btn(boolean in) {
+		byte out = 0;
+		if(in == true) out = 1;
+		return out;
 	}
 	
 }
